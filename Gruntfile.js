f7bfa4da056a6b773d99c4ee65bce49ad7e3f61a
Matthew Revell <matthew@understated.co.uk>
2015-02-04 12:57:25 +0000

    Added HTML replacements, CSS and JS minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,70 @@ module.exports = function(grunt){
   require('load-grunt-tasks')(grunt);    
   
   grunt.initConfig({
+
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'js/**/*.js'],
+      files: ['Gruntfile.js', 'js/main.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
     },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+
+    htmlhint: {
+      options: {},
+      src: [
+        'index.html'
+      ],
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+       src: ['js/**/*.js'],
+       dest: 'js/built.js',
+      },
+    },
+
+    cssc: {
+      csscFirstSet: {
+      files: {
+        'build/css/main.css': 'css/main.css'
+       }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'build/js/built.min.js': ['js/built.js']
+        }
+      }
+    },
+
+    'string-replace': {
+      dist: {
+        files: {
+          'build/index.html': 'index.html'
+        },
+      options: {
+        replacements: [{
+          pattern: '<script src="js/main.js"></script>',
+          replacement: '<script src="js/built.min.js"></script>'
+        },
+        { 
+          pattern: '<script src="js/autonumeric-1.9.30.js"></script>',
+          replacement: ''
+       }]
+      }
     }
+  }
+
+
   });
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'htmlhint', 'concat', 'cssc', 'uglify', 'string-replace']);
 
 };
