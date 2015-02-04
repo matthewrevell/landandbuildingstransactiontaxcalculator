564ee9d29be342fdce6050295a1a8e348d96e4d1
Matthew Revell <matthew@understated.co.uk>
2015-02-04 19:25:48 +0000

    Replaced SDLT code and tests with LBTT

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
-// Calculate the stamp duty due based on house price
+// Calculate the Scottish Land and Buildings Transaction Tax due based on house price
 
-function calcStampDuty(price) {
+function calcLBTT(price) {
 
 var variablePortion = 0;
-var sdlt = 0;
+var lbtt = 0;
 var taxableAmount = 0;
 
-  if (price <= 125000) {
-    // Below £125,000.01, there's no Stamp Duty to pay on residential properties in the UK 
+  if (price <= 145000) {
+    // Below £145,001, there's no LBTT to pay on residential properties in Scotland 
     return 0;   
   }
-  if (price >= 125001 && price <= 250000) {
-    // Between £125,001 and £250,000, 2% is payable on the amount above £125,000
+  if (price >= 145001 && price <= 250000) {
+    // Between £145,001 and £250,000, 2% is payable on the amount above £145,000
     // but below £250,000
-    taxableAmount = price - 125000;
-    sdlt = taxableAmount * 0.02;
-    return Math.floor(sdlt);
+    taxableAmount = price - 145000;
+    lbtt = taxableAmount * 0.02;
+    return Math.floor(lbtt);
   }
-  if (price >= 250001 && price <= 925000) {
-    // Between £250,001 and £925,000, 5% is payable on the amount above £250,000
+  if (price >= 250001 && price <= 325000) {
+    // Between £250,001 and 3925,000, 5% is payable on the amount above £250,000
     variablePortion = price - 250000;
-    sdlt = variablePortion * 0.05 + 2500;
-    return Math.floor(sdlt);
+    lbtt = variablePortion * 0.05 + 2100;
+    return Math.floor(lbtt);
   }
-  if (price >= 925001 && price <= 1500000) {
-    // Between £925,001 and £1,500,000, 10% is payable on the amount above £925,000
-    variablePortion = price - 925000;
-    sdlt = variablePortion * 0.1 + 36250;
-    return Math.floor(sdlt);
+  if (price >= 325001 && price <= 750000) {
+    // Between £325,001 and £750,000, 10% is payable on the amount above £325,000
+    variablePortion = price - 325000;
+    lbtt = variablePortion * 0.1 + 5850;
+    return Math.floor(lbtt);
   }
-  if (price > 1500000) {
-    // Over £1.5m, 12% is payable for that portion
-    variablePortion = price - 1500000;
-    sdlt = variablePortion * 0.12 + 93750;
-    return Math.floor(sdlt);
+  if (price > 750000) {
+    // Over £750k, 12% is payable for that portion
+    variablePortion = price - 750000;
+    lbtt = variablePortion * 0.12 + 48350;
+    return Math.floor(lbtt);
   }
 }
 
@@ -47,7 +47,7 @@ $(document).ready(function() {
       $('#result').removeClass("hide-text");
     }
     var price = $('#houseprice').autoNumeric('get');
-    var duty = calcStampDuty(price); 
+    var duty = calcLBTT(price); 
     $('#result-figure').autoNumeric('set',duty);
   });
   // Simulate user click when Enter is press on the house price input
--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,27 @@
-QUnit.test('Amounts under £125,001 have 0 SDLT', function(assert) {
-  assert.strictEqual(calcStampDuty(1), 0, '1 should give 0 SDLT');
-  assert.strictEqual(calcStampDuty(79635), 0, '79635 should give 0 SDLT');
-  assert.strictEqual(calcStampDuty(125000), 0, '125000 should give 0 SDLT');
+QUnit.test('Amounts under £125,001 have 0 LBTT', function(assert) {
+  assert.strictEqual(calcLBTT(1), 0, '1 should give 0 LBTT');
+  assert.strictEqual(calcLBTT(79635), 0, '79635 should give 0 LBTT');
+  assert.strictEqual(calcLBTT(125000), 0, '125000 should give 0 LBTT');
 });
 
 QUnit.test('Amounts between £125,001 and £250k (allowing for rounding)', function(assert) {
-  assert.strictEqual(calcStampDuty(125001), 0, '125,001 should give 0 SDLT');
-  assert.strictEqual(calcStampDuty(125049), 0, '125,049 should give 0 SDLT');
-  assert.strictEqual(calcStampDuty(125050), 1, '125,050 should give 1 SDLT');
-  assert.strictEqual(calcStampDuty(250000), 2500, '250,000 should give 2,500 SDLT');
+  assert.strictEqual(calcLBTT(125001), 0, '125,001 should give 0 LBTT');
+  assert.strictEqual(calcLBTT(125049), 0, '125,049 should give 0 LBTT');
+  assert.strictEqual(calcLBTT(125050), 0, '125,050 should give 1 LBTT');
+  assert.strictEqual(calcLBTT(250000), 2100, '250,000 should give 2,100 LBTT');
 });
 
-QUnit.test('Amounts between £250,001 and £925k ', function(assert) {
-  assert.strictEqual(calcStampDuty(250001), 2500, '£250,001 should give £2500 SDLT');
-  assert.strictEqual(calcStampDuty(925000), 36250, '£925,000 should give £36,250 SDLT');
+QUnit.test('Amounts between £250,001 and £325k ', function(assert) {
+  assert.strictEqual(calcLBTT(250001), 2100, '£250,001 should give £2100 LBTT');
+  assert.strictEqual(calcLBTT(325000), 5850, '£325,000 should give £5,850 LBTT');
 });
 
-QUnit.test('Amounts between £925,001 and £1.5m', function(assert) {
-  assert.strictEqual(calcStampDuty(925001), 36250, '£925,001 should give £36,250 SDLT');
-  assert.strictEqual(calcStampDuty(1500000), 93750, '£1,500,000 should give £93,750 SDLT');
+QUnit.test('Amounts between £325,001 and £750k', function(assert) {
+  assert.strictEqual(calcLBTT(325001), 5850, '£325,001 should give £5,850 LBTT');
+  assert.strictEqual(calcLBTT(750000), 48350, '£750,000 should give £48,350 LBTT');
 });
 
-QUnit.test('Amounts over £1.5m', function(assert) {
-  assert.strictEqual(calcStampDuty(1500001), 93750, '£1500001 should give £93,750 SDLT');
-  assert.strictEqual(calcStampDuty(99000000), 11793750, '£99,000,000 should give £11,793,750 SDLT');
+QUnit.test('Amounts over £750k', function(assert) {
+  assert.strictEqual(calcLBTT(750001), 48350, '£750001 should give £48,350 LBTT');
+  assert.strictEqual(calcLBTT(99000000), 11838350, '£99,000,000 should give £11,838,350 LBTT');
 });
